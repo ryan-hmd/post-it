94f498d1fbc573a3caefb271f839b11b43afb9f7
ryanhmd <ryanhmd.pro@gmail.com>
2025-04-11 05:18:54 +0200

    feat: define notification model & related types

---
+++ app/models/notification.ts
@@ -0,0 +1,32 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+import type { NotificationStatus } from '../../types/enums.js'
+
+export default class Notification extends BaseModel {
+    @column({ isPrimary: true })
+    declare id: number
+
+    @column()
+    declare userId: string
+
+    @column()
+    declare href?: string
+
+    @column()
+    declare status: NotificationStatus
+
+    @column()
+    declare title: string
+
+    @column()
+    declare description?: string
+
+    @column()
+    declare opened: boolean
+
+    @column.dateTime({ autoCreate: true })
+    declare createdAt: DateTime
+
+    @column.dateTime({ autoCreate: true, autoUpdate: true })
+    declare updatedAt: DateTime
+}
