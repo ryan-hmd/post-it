ed7543e62b1e66f37c2fb290a50571cd9c0a92ed
Ryan Hmd <ryanhmd.pro@gmail.com>
2025-04-16 15:54:06 +0200

    feat: define relationship of Postit w/ itself

--- app/models/postit.ts
+++ app/models/postit.ts
@@ -1,6 +1,6 @@
 import { DateTime } from 'luxon'
-import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
-import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import { BaseModel, belongsTo, column, hasMany } from '@adonisjs/lucid/orm'
+import type { BelongsTo, HasMany } from '@adonisjs/lucid/types/relations'
 import User from './user.js'
 import Wall from './wall.js'
 
@@ -43,6 +43,12 @@ export default class Postit extends BaseModel {
         foreignKey: 'authorId',
     })
     declare author: BelongsTo<typeof User>
+
+    @hasMany(() => Postit)
+    declare responses: HasMany<typeof Postit>
+
+    @belongsTo(() => Postit)
+    declare root: BelongsTo<typeof Postit>
 
     @belongsTo(() => Wall)
     declare wall: BelongsTo<typeof Wall>
