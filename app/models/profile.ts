039980952b01936941bcdacacf1ef6ad0d3dc777
ryanhmd <ryanhmd.pro@gmail.com>
2025-04-23 23:06:05 +0200

    refactor: add countryCode property and related enumerator

--- app/models/profile.ts
+++ app/models/profile.ts
@@ -1,6 +1,7 @@
 import { DateTime } from 'luxon'
 import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
 import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import type { CountryCode } from '#enums/country'
 import User from './user.js'
 
 export default class Profile extends BaseModel {
@@ -15,6 +16,9 @@ export default class Profile extends BaseModel {
 
     @column()
     declare lastName?: string
+
+    @column()
+    declare countryCode?: CountryCode
 
     @column()
     declare pictureURL?: string
