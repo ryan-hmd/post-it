29377886fff47323f5e6f18c8588a7f8a78b6c72
ryanhmd <ryanhmd.pro@gmail.com>
2025-04-23 23:58:01 +0200

    feat: add migration for profiles table

---
+++ database/migrations/1745411405511_create_profiles_table.ts
@@ -0,0 +1,22 @@
+import { countryCodes } from '#enums/country'
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+    protected tableName = 'profiles'
+
+    async up() {
+        this.schema.createTable(this.tableName, (table) => {
+            table.increments('id').primary()
+            table.integer('user_id').unsigned().references('users.id').onDelete('CASCADE')
+            table.string('first_name', 32).defaultTo(null)
+            table.string('last_name', 32).defaultTo(null)
+            table.enum('country_code', countryCodes).defaultTo(null)
+            table.string('picture_url').defaultTo(null)
+            table.timestamp('updated_at')
+        })
+    }
+
+    async down() {
+        this.schema.dropTable(this.tableName)
+    }
+}
