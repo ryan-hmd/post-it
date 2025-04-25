a4f4a09e7b193c37e3657d53219441f0ce3d3776
ryanhmd <ryanhmd.pro@gmail.com>
2025-04-25 17:44:12 +0200

    feat: add migration for members table

---
+++ database/migrations/1745595446434_create_members_table.ts
@@ -0,0 +1,19 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+    protected tableName = 'members'
+
+    async up() {
+        this.schema.createTable(this.tableName, (table) => {
+            table.increments('id')
+            table.integer('user_id').unsigned().references('users.id').onDelete('CASCADE')
+            table.integer('wall_id').unsigned().references('walls.id').onDelete('CASCADE')
+            table.integer('role_id').unsigned().references('roles.id').onDelete('SET NULL')
+            table.timestamp('created_at')
+        })
+    }
+
+    async down() {
+        this.schema.dropTable(this.tableName)
+    }
+}
