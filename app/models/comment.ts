37847c1cd72ab040dd35b514a6996532ed24fc9e
ryanhmd <ryanhmd.pro@gmail.com>
2025-04-25 17:27:51 +0200

    fix: remove useless relation with wall

--- app/models/comment.ts
+++ app/models/comment.ts
@@ -3,7 +3,6 @@ import { BaseModel, belongsTo, column, hasMany } from '@adonisjs/lucid/orm'
 import type { BelongsTo, HasMany } from '@adonisjs/lucid/types/relations'
 import Postit from './postit.js'
 import User from './user.js'
-import Wall from './wall.js'
 
 export default class Comment extends BaseModel {
     @column({ isPrimary: true })
@@ -46,20 +45,17 @@ export default class Comment extends BaseModel {
     declare author: BelongsTo<typeof User>
 
     @belongsTo(() => Postit, {
-        foreignKey: 'threadId'
+        foreignKey: 'threadId',
     })
     declare thread: BelongsTo<typeof Postit>
 
     @hasMany(() => Comment, {
-        foreignKey: 'parentCommentId'
+        foreignKey: 'parentCommentId',
     })
     declare replies: HasMany<typeof Comment>
 
     @belongsTo(() => Comment, {
-        foreignKey: 'parentCommentId'
+        foreignKey: 'parentCommentId',
     })
     declare parentComment: BelongsTo<typeof Comment>
-
-    @belongsTo(() => Wall)
-    declare wall: BelongsTo<typeof Wall>
 }
