03e666e5a00f8eceb57ce8cdf7974e2b453a90f6
ryanhmd <ryanhmd.pro@gmail.com>
2025-04-25 18:01:11 +0200

    feat: add migration for notifications table

---
+++ database/migrations/1745595883892_create_notifications_table.ts
@@ -0,0 +1,24 @@
+import { notificationStatus } from '#enums/notification'
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+    protected tableName = 'notifications'
+
+    async up() {
+        this.schema.createTable(this.tableName, (table) => {
+            table.increments('id').primary()
+            table.integer('user_id').unsigned().references('users.id').onDelete('CASCADE')
+            table.string('href').defaultTo(null)
+            table.enum('status', notificationStatus).defaultTo(null)
+            table.string('title', 64).notNullable()
+            table.string('description').defaultTo(null)
+            table.boolean('opened').defaultTo(false)
+            table.timestamp('created_at')
+            table.timestamp('opened_at').defaultTo(null)
+        })
+    }
+
+    async down() {
+        this.schema.dropTable(this.tableName)
+    }
+}
